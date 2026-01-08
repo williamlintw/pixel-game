bf824e0f60f9e8c180d8607523fe577e2c26138d
williamlintw <william.lin.tw@gmail.com>
2026-01-08 10:22:44 +0800

    chore: update fallback gas url

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // 這裡填入您的 Apps Script 網址，用於測試（當環境變數失效時）
-export const FALLBACK_GAS_URL = ""; 
+export const FALLBACK_GAS_URL = "https://script.google.com/macros/s/AKfycbwGdwJwiRGWkUdo0LT-dabvuAlwkOx2CxoO1LYdxv1SVWQwGBNrTEdlYYqm3OyI9qA0/exec"; 
